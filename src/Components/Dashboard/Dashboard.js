33e44740ddd92e94cd1efe44df22746d27494863
Marco <marcocayetanojr@gmail.com>
2020-04-03 09:48:21 -0700

    set up react

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import Product from '../Product/Product';
+
+
+
+class Dashboard extends Component {
+    constructor() {
+        super();
+    }
+
+
+
+
+
+
+
+    render() {
+        return(
+            <div>
+            <div>Dashboard</div>
+            <Product />
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
---
+++ src/Components/Product/Product.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+
+
+class Product extends Component {
+    constructor() {
+        super();
+    }
+
+
+
+
+
+
+
+    render() {
+        return(
+            <div>Product</div>
+        )
+    }
+}
+
+export default Product;
